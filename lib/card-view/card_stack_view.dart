341e2304c4e6ead32f517ea61ede8a7a53838dfc
BuildTools <unconfigured@null.spigotmc.org>
2023-08-28 22:49:46 -0500

    no errors on zoom

--- lib/card-view/card_stack_view.dart
+++ lib/card-view/card_stack_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:flutter/material.dart';
 import '../primatives/card_with_action_view.dart';
+import 'dart:ui';
 
 class CardStackView extends StatelessWidget {
   void Function() openContainerAction;
@@ -16,21 +17,38 @@ class CardStackView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return (Expanded(
-        child: Column(
-      children: [
-        Flexible(
-          child: CardSwiper(
-            cardsCount: cards.length,
-            cardBuilder:
-                (context, index, percentThresholdX, percentThresholdY) =>
-                    cards[index],
-            allowedSwipeDirection:
-                AllowedSwipeDirection.only(left: true, right: true),
-            maxAngle: 0,
-          ),
-        )
-      ],
-    )));
+    return Column(children: [
+      (Expanded(
+          child: Column(
+        children: [
+          // ClipRect(
+          //   child: BackdropFilter(
+          //     filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+          //     child: Container(
+          //       width: 200.0,
+          //       height: 200.0,
+          //       decoration:
+          //           BoxDecoration(color: Colors.grey.shade200.withOpacity(0.5)),
+          //       child: Center(
+          //         child: Text('Frosted',
+          //             style: Theme.of(context).textTheme.displayLarge),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          Flexible(
+            child: CardSwiper(
+              cardsCount: cards.length,
+              cardBuilder:
+                  (context, index, percentThresholdX, percentThresholdY) =>
+                      cards[index],
+              allowedSwipeDirection:
+                  AllowedSwipeDirection.only(left: true, right: true),
+              maxAngle: 0,
+            ),
+          )
+        ],
+      )))
+    ]);
   }
 }
