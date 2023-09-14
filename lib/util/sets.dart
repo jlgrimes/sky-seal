f944c468f7a8831d96bd69a2229e168ed600b595
BuildTools <unconfigured@null.spigotmc.org>
2023-09-14 17:09:34 -0500

    promo load swsh

--- lib/util/sets.dart
+++ lib/util/sets.dart
@@ -1,5 +1,10 @@
 String convertSetCodeToImageUrl(String setCode) {
   if (!setCode.contains('-')) return '';
 
+  if (setCode.contains('swshp')) {
+    final setInfo = setCode.split('-');
+    return 'https://images.pokemontcg.io/${setInfo[0]}/SWSH${setInfo[1]}_hires.png';
+  }
+
   return 'https://images.pokemontcg.io/${setCode.replaceAll('-', '/')}_hires.png';
 }
