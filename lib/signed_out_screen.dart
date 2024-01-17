7fdc3b60242aa8c83f9a7876f53de0b54d9e3af5
BuildTools <unconfigured@null.spigotmc.org>
2024-01-16 23:50:46 -0600

    finally got this thing to work

--- lib/signed_out_screen.dart
+++ lib/signed_out_screen.dart
@@ -5,6 +5,7 @@ import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 /// Function to generate a random 16 character string.
 String _generateRandomString() {
@@ -12,77 +13,31 @@ String _generateRandomString() {
   return base64Url.encode(List<int>.generate(16, (_) => random.nextInt(256)));
 }
 
-signInWithGoogle() async {
+Future<void> _handleSignIn() async {
   try {
-    // Just a random string
-    final rawNonce = _generateRandomString();
-    final hashedNonce = sha256.convert(utf8.encode(rawNonce)).toString();
+    final GoogleSignIn _googleSignIn = GoogleSignIn(
+        serverClientId:
+            '10007324611-d75282u1mb1gmvchotsph99jubcrb9nf.apps.googleusercontent.com');
 
-    /// TODO: update the client ID with your own
-    ///
-    /// Client ID that you registered with Google Cloud.
-    /// You will have two different values for iOS and Android.
-    String clientId = Platform.isIOS
-        ? '10007324611-3q3j2p3m76jguirophir91d7vrgtagcp.apps.googleusercontent.com'
-        : '10007324611-r43e1b65jacughbdkk8ed143cr99879g.apps.googleusercontent.com';
+    final googleUser = await _googleSignIn.signIn();
+    final googleAuth = await googleUser!.authentication;
+    final accessToken = googleAuth.accessToken;
+    final idToken = googleAuth.idToken;
 
-    /// reverse DNS form of the client ID + `:/` is set as the redirect URL
-    final redirectUrl = '${clientId.split('.').reversed.join('.')}:/';
-
-    /// Fixed value for google login
-    const discoveryUrl =
-        'https://accounts.google.com/.well-known/openid-configuration';
-
-    final appAuth = FlutterAppAuth();
-
-    // authorize the user by opening the consent page
-    final result = await appAuth.authorize(
-      AuthorizationRequest(
-        clientId,
-        redirectUrl,
-        discoveryUrl: discoveryUrl,
-        nonce: hashedNonce,
-        scopes: [
-          'openid',
-          'email',
-        ],
-      ),
-    );
-
-    if (result == null) {
-      throw 'Could not find AuthorizationResponse after authorizing';
+    if (accessToken == null) {
+      throw 'No Access Token found.';
     }
-
-    // Request the access and id token to google
-    final tokenResult = await appAuth.token(
-      TokenRequest(
-        clientId,
-        redirectUrl,
-        authorizationCode: result.authorizationCode,
-        discoveryUrl: discoveryUrl,
-        codeVerifier: result.codeVerifier,
-        nonce: result.nonce,
-        scopes: [
-          'openid',
-          'email',
-        ],
-      ),
-    );
-
-    final idToken = tokenResult?.idToken;
-
     if (idToken == null) {
-      throw 'Could not find idToken from the token response';
+      throw 'No ID Token found.';
     }
 
     await Supabase.instance.client.auth.signInWithIdToken(
       provider: Provider.google,
       idToken: idToken,
-      accessToken: tokenResult?.accessToken,
-      nonce: rawNonce,
+      accessToken: accessToken,
     );
-  } catch (e) {
-    debugPrint(e.toString());
+  } catch (error) {
+    print(error);
   }
 }
 
@@ -95,7 +50,7 @@ class SignedOutScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextButton(
-                onPressed: signInWithGoogle, child: Text('Sign in with Google'))
+                onPressed: _handleSignIn, child: Text('Sign in with Google'))
           ],
         ),
       ),
